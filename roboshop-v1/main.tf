1e1ea2b3d419bcc209a33c0425d07b464f584213
srikanthb549 <sbakkolla@gmail.com>
2024-10-20 19:27:56 +0530

    commit

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,17 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.72.1"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-
-resource "aws_instance" "frontend" {
+resource "aws_instance" "testec2" {
   ami			  = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids =["sg-0781e11c5f22234f7"]
+  tags={
+    Name: "testec2"
+  }
 }
