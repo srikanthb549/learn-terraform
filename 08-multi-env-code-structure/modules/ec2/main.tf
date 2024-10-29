ff991be483a7fc3325aff989294e40e246b79239
srikanthb549 <sbakkolla@gmail.com>
2024-10-29 11:08:23 +0530

    commit3

--- 08-multi-env-code-structure/modules/ec2/main.tf
+++ 08-multi-env-code-structure/modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  vpc_security_group_ids = ["sg-0781e11c5f22234f7"]
   tags = {
     Name = "test-${var.env}"
   }
