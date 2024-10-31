02e11f946a1c325536c69eec278da50b5c266ca5
srikanthb549 <sbakkolla@gmail.com>
2024-10-31 14:59:56 +0530

    commit5

--- 06-modules/route53/main.tf
+++ 06-modules/route53/main.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "record" {
-  zone_id = "Z02249652EM5BAO495DZ1"
-  name    = "${var.instance_name}-dev.rdevopsb81.online"
+  zone_id = "Z08641142SH8HBQE1FAJT"
+  name    = "${var.instance_name}-dev.sbakkolla.online"
   type    = "A"
   ttl     = "30"
   records = [var.ip_address]
